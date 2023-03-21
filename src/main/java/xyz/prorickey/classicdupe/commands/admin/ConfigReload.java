e2b2dd5315f6a269a96b44fa0a7ff8d3695094ca
Prorickey <trevobedson@gmail.com>
2023-03-20 22:49:52 -0400

    Create ConfigReload.java

---
+++ src/main/java/xyz/prorickey/classicdupe/commands/admin/ConfigReload.java
@@ -0,0 +1,27 @@
+package xyz.prorickey.classicdupe.commands.admin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import xyz.prorickey.classicdupe.Config;
+import xyz.prorickey.classicdupe.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfigReload implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        Config.reloadConfig();
+        sender.sendMessage(Utils.cmdMsg("&aThe config has been reloaded"));
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return new ArrayList<>();
+    }
+}
