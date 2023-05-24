325cb27732c627317aca863b36c6897041ca9a02
Prorickey <trevobedson@gmail.com>
2023-05-23 20:14:23 -0400

    Bye Bye H2

--- src/main/java/xyz/prorickey/classicdupe/database/Database.java
+++ src/main/java/xyz/prorickey/classicdupe/database/Database.java
@@ -3,7 +3,9 @@ package xyz.prorickey.classicdupe.database;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.mariadb.jdbc.export.Prepare;
 import xyz.prorickey.classicdupe.ClassicDupe;
+import xyz.prorickey.classicdupe.Config;
 
 import java.io.File;
 import java.sql.*;
@@ -12,6 +14,7 @@ import java.util.List;
 
 public class Database {
 
+    static Connection conn;
     static Connection playerConn;
     static Connection serverConn;
     static Connection linkingConn;
@@ -23,26 +26,179 @@ public class Database {
 
     public Database() {
         try {
-            playerConn = DriverManager.getConnection ("jdbc:h2:" + ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "playerData");
-            serverConn = DriverManager.getConnection ("jdbc:h2:" + ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "serverData");
-            linkingConn = DriverManager.getConnection ("jdbc:h2:" + ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "linkData");
-            playerConn.prepareStatement("CREATE TABLE IF NOT EXISTS players(uuid varchar, name varchar, nickname varchar, timesjoined long, playtime long, randomitem BOOLEAN, chatcolor VARCHAR, gradient BOOLEAN, gradientfrom VARCHAR, gradientto VARCHAR, night BOOLEAN)").execute();
-            serverConn.prepareStatement("CREATE TABLE IF NOT EXISTS filter(text varchar, fullword BOOLEAN)").execute();
-            serverConn.prepareStatement("CREATE TABLE IF NOT EXISTS spawn(spawn varchar, x DOUBLE, y DOUBLE, z DOUBLE, pitch FLOAT, yaw FLOAT, world varchar)").execute();
-            playerConn.prepareStatement("CREATE TABLE IF NOT EXISTS stats(uuid VARCHAR, kills INT, deaths INT)").execute();
-            playerConn.prepareStatement("CREATE TABLE IF NOT EXISTS particleEffects(uuid VARCHAR, killEffect VARCHAR, particleEffect VARCHAR)").execute();
-            linkingConn.prepareStatement("CREATE TABLE IF NOT EXISTS link(uuid VARCHAR, dscid Long)").execute();
-            filterDatabase = new FilterDatabase(serverConn);
-            playerDatabase = new PlayerDatabase(playerConn);
-            linkingDatabase = new LinkingDatabase(linkingConn);
 
-            // Database Alters - To be removed in the future
-            ResultSet set = playerConn.prepareStatement("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='players' AND COLUMN_NAME='night'").executeQuery();
-            if(!set.next()) playerConn.prepareStatement("ALTER TABLE players" +
-                    " ADD night BOOLEAN NOT NULL" +
-                    " DEFAULT (true)").execute();
+            if(Config.getConfig().getBoolean("database.mariadb")) {
+                conn = DriverManager.getConnection(
+                        "jdbc:mariadb://localhost:3306/classicdupe",
+                        Config.getConfig().getString("database.user"),
+                        Config.getConfig().getString("database.password")
+                );
+
+                System.out.println("Connected to MariaDB database!");
+
+                conn.prepareStatement("CREATE TABLE IF NOT EXISTS players(uuid TEXT, name TEXT, nickname TEXT, timesjoined long, playtime long, randomitem BOOLEAN, chatcolor TEXT, gradient BOOLEAN, gradientfrom TEXT, gradientto TEXT, night BOOLEAN)").execute();
+                conn.prepareStatement("CREATE TABLE IF NOT EXISTS filter(text TEXT, fullword BOOLEAN)").execute();
+                conn.prepareStatement("CREATE TABLE IF NOT EXISTS spawn(spawn TEXT, x DOUBLE, y DOUBLE, z DOUBLE, pitch FLOAT, yaw FLOAT, world TEXT)").execute();
+                conn.prepareStatement("CREATE TABLE IF NOT EXISTS stats(uuid TEXT, kills INT, deaths INT)").execute();
+                conn.prepareStatement("CREATE TABLE IF NOT EXISTS particleEffects(uuid TEXT, killEffect TEXT, particleEffect TEXT)").execute();
+                conn.prepareStatement("CREATE TABLE IF NOT EXISTS link(uuid TEXT, dscid Long)").execute();
+
+                System.out.println("Created tables!");
+
+                if(new File(ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "playerData.mv.db").exists()) {
+
+                    playerConn = DriverManager.getConnection("jdbc:h2:" + ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "playerData");
+
+                    System.out.println("Connected to H2 database!");
+
+                    try(ResultSet set = conn.prepareStatement("SELECT * FROM players").executeQuery()) {
+                        if(!set.next()) {
+                            PreparedStatement playerStat = playerConn.prepareStatement("SELECT * FROM players");
+                            ResultSet playerSet = playerStat.executeQuery();
+                            PreparedStatement stat = conn.prepareStatement("INSERT INTO" +
+                                    " players(uuid, name, nickname, timesjoined, playtime, randomitem, chatcolor, gradient, gradientfrom, gradientto, night)" +
+                                    " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                            while(playerSet.next()) {
+                                stat.setString(1, playerSet.getString("uuid"));
+                                stat.setString(2, playerSet.getString("name"));
+                                stat.setString(3, playerSet.getString("nickname"));
+                                stat.setLong(4, playerSet.getLong("timesjoined"));
+                                stat.setLong(5, playerSet.getLong("playtime"));
+                                stat.setBoolean(6, playerSet.getBoolean("randomitem"));
+                                stat.setString(7, playerSet.getString("chatcolor"));
+                                stat.setBoolean(8, playerSet.getBoolean("gradient"));
+                                stat.setString(9, playerSet.getString("gradientfrom"));
+                                stat.setString(10, playerSet.getString("gradientto"));
+                                stat.setBoolean(11, playerSet.getBoolean("night"));
+                                stat.addBatch();
+                            }
+                            stat.executeLargeBatch();
+                        }
+                    }
+
+                    try(ResultSet set = conn.prepareStatement("SELECT * FROM stats").executeQuery()) {
+                        if(!set.next()) {
+                            PreparedStatement playerStat = playerConn.prepareStatement("SELECT * FROM stats");
+                            ResultSet playerSet = playerStat.executeQuery();
+                            PreparedStatement stat = conn.prepareStatement("INSERT INTO stats(uuid, kills, deaths) VALUES (?, ?, ?)");
+                            while(playerSet.next()) {
+                                stat.setString(1, playerSet.getString("uuid"));
+                                stat.setInt(2, playerSet.getInt("kills"));
+                                stat.setInt(3, playerSet.getInt("deaths"));
+                                stat.addBatch();
+                            }
+                            stat.executeLargeBatch();
+                        }
+                    }
+
+                    try(ResultSet set = conn.prepareStatement("SELECT * FROM particleEffects").executeQuery()) {
+                        if(!set.next()) {
+                            PreparedStatement playerStat = playerConn.prepareStatement("SELECT * FROM particleEffects");
+                            ResultSet playerSet = playerStat.executeQuery();
+                            PreparedStatement stat = conn.prepareStatement("INSERT INTO particleEffects(uuid, killEffect, particleEffect) VALUES (?, ?, ?)");
+                            while(playerSet.next()) {
+                                stat.setString(1, playerSet.getString("uuid"));
+                                stat.setString(2, playerSet.getString("killEffect"));
+                                stat.setString(3, playerSet.getString("particleEffect"));
+                                stat.addBatch();
+                            }
+                            stat.executeLargeBatch();
+                        }
+                    }
+
+                    playerConn.close();
+                    playerConn = conn;
+                    new File(ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "playerData.mv.db").delete();
+
+                }
+
+                if(new File(ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "serverData.mv.db").exists()) {
+
+                    serverConn = DriverManager.getConnection ("jdbc:h2:" + ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "serverData");
+
+                    try(ResultSet set = conn.prepareStatement("SELECT * FROM filter").executeQuery()) {
+                        if(!set.next()) {
+                            PreparedStatement serverStat = serverConn.prepareStatement("SELECT * FROM filter");
+                            ResultSet serverSet = serverStat.executeQuery();
+                            PreparedStatement stat = conn.prepareStatement("INSERT INTO filter(text, fullword) VALUES (?, ?)");
+                            while(serverSet.next()) {
+                                stat.setString(1, serverSet.getString("text"));
+                                stat.setBoolean(2, serverSet.getBoolean("fullword"));
+                                stat.addBatch();
+                            }
+                            stat.executeLargeBatch();
+                        }
+                    }
+
+                    try(ResultSet set = conn.prepareStatement("SELECT * FROM spawn").executeQuery()) {
+                        if(!set.next()) {
+                            PreparedStatement serverStat = serverConn.prepareStatement("SELECT * FROM spawn");
+                            ResultSet serverSet = serverStat.executeQuery();
+                            PreparedStatement stat = conn.prepareStatement("INSERT INTO spawn(spawn, x, y, z, pitch, yaw, world) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                            while(serverSet.next()) {
+                                stat.setString(1, serverSet.getString("spawn"));
+                                stat.setDouble(2, serverSet.getDouble("x"));
+                                stat.setDouble(3, serverSet.getDouble("y"));
+                                stat.setDouble(4, serverSet.getDouble("z"));
+                                stat.setFloat(5, serverSet.getFloat("pitch"));
+                                stat.setFloat(6, serverSet.getFloat("yaw"));
+                                stat.setString(7, serverSet.getString("world"));
+                                stat.addBatch();
+                            }
+                            stat.executeLargeBatch();
+                        }
+                    }
+
+                    serverConn.close();
+                    serverConn = conn;
+                    new File(ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "serverData.mv.db").delete();
+                }
+
+                if(new File(ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "linkData.mv.db").exists()) {
+
+                    linkingConn = DriverManager.getConnection ("jdbc:h2:" + ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "linkData");
+
+                    try (ResultSet set = conn.prepareStatement("SELECT * FROM link").executeQuery()) {
+                        if (!set.next()) {
+                            PreparedStatement linkStat = linkingConn.prepareStatement("SELECT * FROM link");
+                            ResultSet linkSet = linkStat.executeQuery();
+                            PreparedStatement stat = conn.prepareStatement("INSERT INTO link(uuid, dscid) VALUES (?, ?)");
+                            while (linkSet.next()) {
+                                stat.setString(1, linkSet.getString("uuid"));
+                                stat.setLong(2, linkSet.getLong("dscid"));
+                                stat.addBatch();
+                            }
+                            stat.executeLargeBatch();
+                        }
+                    }
+
+                    linkingConn.close();
+                    linkingConn = conn;
+                    new File(ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "linkData.mv.db").delete();
+                }
+
+                filterDatabase = new FilterDatabase(serverConn);
+                playerDatabase = new PlayerDatabase(playerConn);
+                linkingDatabase = new LinkingDatabase(linkingConn);
+
+            } else {
+                playerConn = DriverManager.getConnection ("jdbc:h2:" + ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "playerData");
+                serverConn = DriverManager.getConnection ("jdbc:h2:" + ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "serverData");
+                linkingConn = DriverManager.getConnection ("jdbc:h2:" + ClassicDupe.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "linkData");
+
+                playerConn.prepareStatement("CREATE TABLE IF NOT EXISTS players(uuid varchar, name varchar, nickname varchar, timesjoined long, playtime long, randomitem BOOLEAN, chatcolor VARCHAR, gradient BOOLEAN, gradientfrom VARCHAR, gradientto VARCHAR, night BOOLEAN)").execute();
+                serverConn.prepareStatement("CREATE TABLE IF NOT EXISTS filter(text varchar, fullword BOOLEAN)").execute();
+                serverConn.prepareStatement("CREATE TABLE IF NOT EXISTS spawn(spawn varchar, x DOUBLE, y DOUBLE, z DOUBLE, pitch FLOAT, yaw FLOAT, world varchar)").execute();
+                playerConn.prepareStatement("CREATE TABLE IF NOT EXISTS stats(uuid VARCHAR, kills INT, deaths INT)").execute();
+                playerConn.prepareStatement("CREATE TABLE IF NOT EXISTS particleEffects(uuid VARCHAR, killEffect VARCHAR, particleEffect VARCHAR)").execute();
+                linkingConn.prepareStatement("CREATE TABLE IF NOT EXISTS link(uuid VARCHAR, dscid Long)").execute();
+
+                filterDatabase = new FilterDatabase(serverConn);
+                playerDatabase = new PlayerDatabase(playerConn);
+                linkingDatabase = new LinkingDatabase(linkingConn);
+            }
         } catch (SQLException e) {
-            Bukkit.getLogger().severe(e.toString());
+            throw new RuntimeException(e);
         }
         spawn = getSpawn();
     }
