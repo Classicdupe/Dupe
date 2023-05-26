9681323270243b5d717c8a9be52a9611ec737f54
BraveDishDealer <86237436+BraveDishDealer@users.noreply.github.com>
2023-05-26 20:12:28 +0300

    Fixed gapple cooldown

--- src/main/java/xyz/prorickey/classicdupe/events/GoldenAppleCooldown.java
+++ src/main/java/xyz/prorickey/classicdupe/events/GoldenAppleCooldown.java
@@ -16,11 +16,7 @@ public class GoldenAppleCooldown implements Listener {
     public void onPlayerItemConsume(PlayerItemConsumeEvent e) {
         if(e.getItem().getType() != Material.ENCHANTED_GOLDEN_APPLE) return;
         if(e.getPlayer().hasCooldown(Material.ENCHANTED_GOLDEN_APPLE))e.getPlayer().sendMessage(Utils.format("<red>Enchanted golden apples are in cooldown."));
-        int cooldown = 2200;
-        if(e.getPlayer().hasPermission("perks.godAppleCooldown.vip")) cooldown = 1000;
-        else if(e.getPlayer().hasPermission("perks.godAppleCooldown.mvp")) cooldown = 800;
-        else if(e.getPlayer().hasPermission("perks.godAppleCooldown.legend")) cooldown = 600;
-        e.getPlayer().setCooldown(Material.ENCHANTED_GOLDEN_APPLE, cooldown);
+        e.getPlayer().setCooldown(Material.ENCHANTED_GOLDEN_APPLE, 60*20);
     }
 
 }
