77cdb6a46bc0af27622ed5ab5b836ea2f80ffe0b
Prorickey <trevobedson@gmail.com>
2023-06-15 19:19:39 -0400

    Removed unused imports

--- src/main/java/xyz/prorickey/classicdupe/custom/tables/CraftingTable.java
+++ src/main/java/xyz/prorickey/classicdupe/custom/tables/CraftingTable.java
@@ -1,10 +1,8 @@
 package xyz.prorickey.classicdupe.custom.tables;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
